c5d23ae9fa7d240b0a3fd44d095e6e202370e7d3
TheDoubleL0215 <levai.levente.gabor@gmail.com>
2023-11-12 09:44:35 +0100

    fontos hibajavítás a fájl olvasási feltételekben

--- src/public/js/tanar.js
+++ src/public/js/tanar.js
@@ -13,40 +13,86 @@ document.getElementById('saveBtn').addEventListener('click', (e)=>{
 
         var sorok = content.split('\n')
 
+        var sorSzám = 0
+
 
         //itt csekklojuk a 
         sorok.forEach(function(sor) {
+            sorSzám++
             console.log("!!!!!!!Ez itt egy ártatlan szelíd sor: ", sor);
 
             console.log("Ez a content sor: ", sor);
 
-
             if(wordCounter(sor) == 5){
-                if(Number.isInteger(parseInt(sor.split(' ')[4])) == true){
-                    if(parseInt(sor.split(' ')[4]) > 4 && parseInt(sor.split(' ')[4]) < 9){
-                        console.log("ÁTMENT!!!")
 
-                        //itt van az hogy a sorral minden rendbe, csak be kell rakni az adatbázisba
+                if(Number.isInteger(parseInt(sor.split(' ')[4])) == true){
+
+                    if(parseInt(sor.split(' ')[4]) > 4 && parseInt(sor.split(' ')[4]) < 9){
+
+                        if(maganhangzoSzamolas(sor.split(' ')[3]) > 2){
+
+                            console.log("ÁTMENT!!!")
+
+                            //itt van az hogy a sorral minden rendbe, csak be kell rakni az adatbázisba
+
+                        }else{
+                            //TODO: hibakezelés a nem min. 3 magánhangzóra
+                            console.log("//TODO: hibakezelés a nem min. 3 magánhangzóra")
+                            const toastTrigger = document.getElementById('saveBtn')
+                            const toastLiveExample = document.getElementById('wrongImportFormatNotify')
+                            const message = document.getElementById('notifyMessage')
+                
+                            const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                            message.innerHTML = 'Az egyik sorban az utolsó szó nem tartalmaz min. 3 magánhangzót'
+                            toastBootstrap.show()
+                        }
 
 
                     }else{
                         //TODO: hibakezelés a nem 5 - 8ig osztályokra
                         console.log("//TODO: hibakezelés a nem 5 - 8ig osztályokra")
+                        const toastTrigger = document.getElementById('saveBtn')
+                        const toastLiveExample = document.getElementById('wrongImportFormatNotify')
+                        const message = document.getElementById('notifyMessage')
+            
+                        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                        message.innerHTML = 'Az egyik sorban a szám nem 5 és 8 között van'
+                        toastBootstrap.show()
                     }
 
                 }else{
-                    //TODO: hibakezelés hogy a harmadik szó nem szám
-                    console.log("//TODO: hibakezelés hogy a harmadik szó nem szám")
+                    //TODO: hibakezelés hogy a negyedik szó nem szám
+                    console.log("//TODO: hibakezelés hogy a negyedik szó nem szám")
+                    const toastTrigger = document.getElementById('saveBtn')
+                    const toastLiveExample = document.getElementById('wrongImportFormatNotify')
+                    const message = document.getElementById('notifyMessage')
+        
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                    message.innerHTML = 'Az egyik sorban a negyedik elem nem szám'
+                    toastBootstrap.show()
                 }
-                    if(maganhangzoSzamolas(sor.split(' ')[3]) > 2){
 
-                    }else{
-                        //TODO: hibakezelés a nem min. 3 magánhangzóra
-                        console.log("//TODO: hibakezelés a nem min. 3 magánhangzóra")
-                    }
             }else{
-                // TODO: hibakezelés a túl rövid sorra
-                console.log("// TODO: hibakezelés a túl rövid sorra")
+                if(sorSzám > 1 && sor == ""){
+                    console.log("Ez a sorSzám debug false")
+                    const toastTrigger = document.getElementById('saveBtn')
+                    const toastLiveExample = document.getElementById('wrongImportFormatNotify')
+                    const message = document.getElementById('notifyMessage')
+        
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                    message.innerHTML = 'A fájlban található üres sor(ok)'
+                    toastBootstrap.show()
+                }else{
+                    // TODO: hibakezelés a túl rövid sorra
+                    console.log("// TODO: hibakezelés a túl rövid sorra")
+                    const toastTrigger = document.getElementById('saveBtn')
+                    const toastLiveExample = document.getElementById('wrongImportFormatNotify')
+                    const message = document.getElementById('notifyMessage')
+        
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                    message.innerHTML = 'Az egyik sor nem felel meg a megfeleő formátumnak <br> (szó, szó, szó, szó, évfolyam)'
+                    toastBootstrap.show()
+                }
             }
           });
 
